0e7979aeea7ec97d099184c79124b8717a87c446
JJ <jjmonzon@prontobpo.com>
2024-09-04 15:52:30 -0600

    #add se agregaron colores

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Prueba from "./view/Prueba"
 import Calendario from "./view/Calendario"
 import Medicion from "./view/Medicion"
 import Usuario from "./view/Usuario"
+import { Col, Row } from 'antd';
+import { magenta } from '@ant-design/colors';
 function App() {
   return (
     <>
@@ -11,6 +13,24 @@ function App() {
       <Calendario />
       <Medicion />
       <Usuario />
+      <Row>
+      <Col span={24}>col</Col>
+    </Row>
+    <Row>
+      <Col span={12} style={{ backgroundColor: magenta[3] }}>col-12</Col>
+      <Col span={12}>col-12</Col>
+    </Row>
+    <Row>
+      <Col span={8}>col-8</Col>
+      <Col span={8}>col-8</Col>
+      <Col span={8}>col-8</Col>
+    </Row>
+    <Row>
+      <Col span={6}>col-6</Col>
+      <Col span={6}>col-6</Col>
+      <Col span={6}>col-6</Col>
+      <Col span={6}>col-6</Col>
+    </Row>
       
     </>
   )
